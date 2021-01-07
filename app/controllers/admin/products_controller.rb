a9fd6ac1e544b48e8a7a4d39be371a46be80d77c
Kelvin <tobar411@gmail.com>
2021-01-07 21:35:07 +0000

    remove exception from authenication

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,6 +1,6 @@
 class Admin::ProductsController < ApplicationController
 
-  http_basic_authenticate_with :name => ENV['JUNGLE_ADMIN_NAME'], :password => ENV["JUNGLE_ADMIN_PASSWORD"], :except => :index
+  http_basic_authenticate_with :name => ENV['JUNGLE_ADMIN_NAME'], :password => ENV["JUNGLE_ADMIN_PASSWORD"]
 
   def index
     @products = Product.order(id: :desc).all
